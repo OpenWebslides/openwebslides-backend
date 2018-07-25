c62c11b423b3fed512854cc4fdb369c18b3a5a5f
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-07-25 08:34:11 +0200

    Add versioning concern spec

--- app/controllers/concerns/versioning.rb
+++ app/controllers/concerns/versioning.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+MEDIA_TYPE = 'application/vnd.openwebslides+json'
+
 module Versioning
   extend ActiveSupport::Concern
 
@@ -8,15 +10,18 @@ module Versioning
     before_action :verify_accept_header_version
   end
 
+  # Verify if the current API version can satisfy the API version in the request's Accept header
   def verify_accept_header_version
     media_types = HTTP::Accept::MediaTypes.parse request.accept
     constraint = Semverse::Constraint.new "~> #{OpenWebslides.config.api.version}"
 
-    request_version = Semverse::Version.new media_types.find { |mt| mt.parameters['version'] }
-                                                       .parameters['version']
+    # Select media types of format 'application/vnd.openwebslides+json; version=...'
+    request_versions = media_types.select { |mt| mt.mime_type == MEDIA_TYPE && mt.parameters['version'] }
+                                  .map { |mt| mt.parameters['version'] }
 
-    return true if constraint.satisfies? request_version
+    # Check if version can be satisfied
+    return true if request_versions.any? { |v| constraint.satisfies? v }
 
-    raise JSONAPI::Exceptions::UnacceptableVersionError, request_version.to_s
+    raise JSONAPI::Exceptions::UnacceptableVersionError, request_versions.join(',')
   end
 end
