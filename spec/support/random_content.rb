0d25bc7ce6a67dc39b068f72bf899e85bbea887c
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-09-01 10:03:29 +0200

    Add random content generator

---
+++ spec/support/random_content.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+include OpenWebslides::Converter::Content::Properties::Identifier
+
+def random_content(size = 10)
+  content = []
+
+  # Root content item
+  root = {
+    :id => generate_id,
+    :type => 'contentItemTypes/ROOT',
+    :childItemIds => []
+  }
+
+  # Add root to content
+  content << root
+
+  size.times.each do
+    content_item = if [true, false].sample
+                     {
+                       :id => generate_id,
+                       :type => 'contentItemTypes/PARAGRAPH',
+                       :text => Faker::Lorem.words(60).join(' ')
+                     }
+                   else
+                     {
+                       :id => generate_id,
+                       :type => 'contentItemTypes/HEADING',
+                       :text => Faker::Lorem.words(8).join(' ')
+                     }
+                   end
+
+    # Add content item to content and reference from root content item
+    root[:childItemIds] << content_item[:id]
+    content << content_item
+  end
+
+  # Return content
+  content
+end
