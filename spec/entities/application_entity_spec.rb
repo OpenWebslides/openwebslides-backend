876c45f3caef1f4836f4603537988b5a2ba1e4e3
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-09-01 12:49:14 +0200

    Add ApplicationEntity constructor

--- spec/entities/application_entity_spec.rb
+++ spec/entities/application_entity_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe ApplicationEntity, :type => :model do
   # Test variables
   #
   class TestEntity < ApplicationEntity
-    property :my_property
+    property :my_attribute
     belongs_to :my_association
   end
 
@@ -26,15 +26,27 @@ RSpec.describe ApplicationEntity, :type => :model do
   # Tests
   #
   describe 'methods' do
-    describe '.property' do
-      describe 'adds a property' do
-        it { is_expected.to respond_to :my_property }
-        it { is_expected.to respond_to :my_property= }
+    describe '#initialize' do
+      it 'assigns attributes' do
+        test_entity = TestEntity.new :my_attribute => 'foobar'
+
+        expect(test_entity.my_attribute).to eq 'foobar'
+      end
+
+      it 'raises UnknownAttributeError on uknown attribute' do
+        expect { TestEntity.new :foo => 'bar' }.to raise_error ActiveModel::UnknownAttributeError
+      end
+    end
+
+    describe '.attribute' do
+      describe 'adds a attribute' do
+        it { is_expected.to respond_to :my_attribute }
+        it { is_expected.to respond_to :my_attribute= }
       end
     end
 
     describe '.belongs_to' do
-      describe 'adds a property and validates presence' do
+      describe 'adds a attribute and validates presence' do
         it { is_expected.to respond_to :my_association }
         it { is_expected.to respond_to :my_association= }
         it { is_expected.to validate_presence_of :my_association }
