9f65be4e781e89cab963b8a7bccf8a70e1835c5e
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-10-02 13:46:53 +0200

    Add low-level Fork commands

---
+++ app/commands/repository/filesystem/copy.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'fileutils'
+
+module Repository
+  module Filesystem
+    ##
+    # Duplicate repository
+    #
+    class Copy < YAMLCommand
+      attr_accessor :fork
+
+      def execute
+        raise OpenWebslides::ArgumentError, 'No fork specified' unless @fork
+
+        # Create initial directory
+        raise OpenWebslides::RepoExistsError if Dir.exist? repo_path(@fork)
+        FileUtils.mkdir_p repo_path(@fork)
+
+        # Recursively copy repository
+        FileUtils.cp_r File.join(repo_path, '.'), repo_path(@fork)
+      end
+    end
+  end
+end
