388a0a25eb234d8c7325de7aef4e9495290f2264
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-10-02 18:11:38 +0200

    AddDummyType: add support for create action

--- app/controllers/concerns/add_dummy_id.rb
+++ app/controllers/concerns/add_dummy_id.rb
@@ -6,15 +6,26 @@ module AddDummyId
   # Add a dummy `id` attribute to the params, because JSONAPI::Resources does not officially
   # support singleton resources, and requires and `id` to be present when modifying these
   #
-  # Prepend this action to the callback chain:
+
+  ##
+  # Prepend this action to the callback chain when creating a singleton:
   #
-  # prepend_before_action :add_dummy_update_id, :only => %i[update]
-  def add_dummy_update_id
+  # prepend_before_action :add_dummy_create_id, :only => %i[update]
+  def add_dummy_create_id
     params[:data] ||= {}
     params[:data][:id] ||= 0
   end
 
-  # Prepend this action to the callback chain:
+  ##
+  # Prepend this action to the callback chain when updating a singleton:
+  #
+  # prepend_before_action :add_dummy_update_id, :only => %i[update]
+  def add_dummy_update_id
+    add_dummy_create_id
+  end
+
+  ##
+  # Prepend this action to the callback chain when destroying a singleton:
   #
   # prepend_before_action :add_dummy_destroy_id, :only => %i[destroy]
   def add_dummy_destroy_id
