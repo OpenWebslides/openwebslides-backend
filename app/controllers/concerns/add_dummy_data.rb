2f6cbaf3159912de0732d09f771ec3f4de7605a2
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-10-02 18:23:20 +0200

    AddDummyData: support derived types

--- app/controllers/concerns/add_dummy_data.rb
+++ app/controllers/concerns/add_dummy_data.rb
@@ -3,6 +3,7 @@
 module AddDummyData
   extend ActiveSupport::Concern
 
+  ##
   # Add a dummy `id` attribute to the params, because JSONAPI::Resources does not officially
   # support singleton resources, and requires and `id` to be present when modifying these
   #
@@ -10,7 +11,8 @@ module AddDummyData
   ##
   # Prepend this action to the callback chain when creating a singleton:
   #
-  # prepend_before_action :add_dummy_create_id, :only => %i[update]
+  # prepend_before_action :add_dummy_create_id, :only => %i[create]
+  #
   def add_dummy_create_id
     params[:data] ||= {}
     params[:data][:id] ||= 0
@@ -20,6 +22,7 @@ module AddDummyData
   # Prepend this action to the callback chain when updating a singleton:
   #
   # prepend_before_action :add_dummy_update_id, :only => %i[update]
+  #
   def add_dummy_update_id
     add_dummy_create_id
   end
@@ -28,7 +31,23 @@ module AddDummyData
   # Prepend this action to the callback chain when destroying a singleton:
   #
   # prepend_before_action :add_dummy_destroy_id, :only => %i[destroy]
+  #
   def add_dummy_destroy_id
     params[:id] ||= 0
+  end
+
+  ##
+  # Add a `type` attribute to the params, because JSONAPI::Resources requires a type,
+  # even when handling POST resources without body
+  #
+
+  ##
+  # Prepend this action to the callback chain when creating a body-less resource:
+  #
+  # prepend_before_action :add_dummy_type, :only => %i[create]
+  #
+  def add_dummy_type
+    params[:data] ||= {}
+    params[:data][:type] = resource_klass._type.to_s
   end
 end
