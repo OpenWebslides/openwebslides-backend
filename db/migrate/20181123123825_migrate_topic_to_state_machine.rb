73328ee29d9b85e780f0edab74ceda6a79f6d9a7
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-11-23 12:49:41 +0000

    Migrate Topic state to access in database

---
+++ db/migrate/20181123123825_migrate_topic_to_state_machine.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class MigrateTopicToStateMachine < ActiveRecord::Migration[5.2]
+  def change
+    Topic.transaction do
+      add_column :topics, :access, :string, :null => false, :default => ''
+
+      map_state_to_access = {
+        'public_access' => 'public',
+        'protected_access' => 'protected',
+        'private_access' => 'private'
+      }
+
+      Topic.all.each do |topic|
+        topic.update! :access => map_state_to_access[topic.state]
+
+        topic.reload
+
+        raise 'Value is not equal to old value' unless topic.attributes_before_type_cast['access'] == map_state_to_access[topic.state]
+        raise 'Value is not legal' unless %w[public protected private].include?(topic.access)
+      end
+
+      remove_column :topics, :state
+    end
+  end
+end
