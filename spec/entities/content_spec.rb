0ce9146ca70c0aa727456b18e75e20e1104ad58e
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-12-13 17:55:46 +0100

    Refactor content_spec

--- spec/entities/content_spec.rb
+++ spec/entities/content_spec.rb
@@ -10,35 +10,21 @@ RSpec.describe Content, :type => :model do
   # Stubs and mocks
   #
   ##
-  # Test variables
-  #
-  subject { content }
-
-  let(:content) { build :content }
-
-  ##
   # Subject
   #
+  subject(:content) { build :content }
 
+  ##
+  # Test variables
+  #
   ##
   # Tests
   #
   describe 'attributes' do
-    it { is_expected.not_to allow_value(nil).for(:content) }
-    it { is_expected.not_to allow_value('').for(:content) }
-
-    it 'is invalid without attributes' do
-      expect(described_class.new).not_to be_valid
-    end
-
-    it 'is valid with valid attributes' do
-      expect(build :content).to be_valid
-    end
+    it { is_expected.to validate_presence_of :content }
   end
 
   describe 'associations' do
-    it 'is not valid without topic' do
-      expect(build :content, :topic => nil).not_to be_valid
-    end
+    it { is_expected.to validate_presence_of :topic }
   end
 end
