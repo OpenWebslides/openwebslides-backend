4f7037b6f4ccebfef68e5f61dffa9398a1f4cb2f
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-03-02 13:32:10 -0300

    Add TokenPolicy#update?; check for user presence

--- app/policies/token_policy.rb
+++ app/policies/token_policy.rb
@@ -5,10 +5,17 @@ class TokenPolicy < ApplicationPolicy
   # Resource
   #
   def create?
+    # Everyone can create a token
     true
   end
 
+  def update?
+    # Users can update a token
+    !@user.nil?
+  end
+
   def destroy?
-    true
+    # Users can update a token
+    update?
   end
 end
