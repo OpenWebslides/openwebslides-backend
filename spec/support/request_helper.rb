24fc45e1a1d1e99472978c80fc9350ba127b8e9a
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-03-04 13:31:32 -0300

    Rewrite RequestHelper

--- spec/support/request_helper.rb
+++ spec/support/request_helper.rb
@@ -1,17 +1,30 @@
 # frozen_string_literal: true
 
 module RequestHelper
-  def headers
-    @headers ||= {
+  ##
+  # Generate request headers
+  #
+  def headers(*headers)
+    # Auto-generated headers
+    request_headers = {
+      'Content-Type' => JSONAPI::MEDIA_TYPE,
       'Accept' => "application/vnd.api+json, application/vnd.openwebslides+json; version=#{OpenWebslides.config.api.version}"
     }
-  end
 
-  def add_content_type_header
-    (@headers ||= headers)['Content-Type'] = 'application/vnd.api+json'
-  end
+    # Custom headers
+    headers.each do |header|
+      case header
+      when :refresh
+        request_headers['Authorization'] = "Bearer #{JWT::Auth::RefreshToken.new(:subject => user).to_jwt}"
+      when :access
+        request_headers['Authorization'] = "Bearer #{JWT::Auth::AccessToken.new(:subject => user).to_jwt}"
+      when nil
+        next
+      else
+        raise "Header not recognized: #{header}"
+      end
+    end
 
-  def add_auth_header
-    (@headers ||= headers)['Authorization'] = "Bearer #{JWT::Auth::Token.from_user(user).to_jwt}"
+    request_headers
   end
 end
