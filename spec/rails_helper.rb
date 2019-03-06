5158a6d81e9df93fb600ec4365a224357b95cae6
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-03-06 09:24:38 -0300

    Remove ResponseHelper; rename RequestHelper to Headers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,8 +36,7 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.include RequestHelper
-  config.include ResponseHelper
+  config.include Headers
   config.include StateMachinesRspec::Matchers
   config.include OpenWebslides::Matchers
 
--- spec/support/headers.rb
+++ spec/support/headers.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module RequestHelper
+module Headers
   ##
   # Generate request headers
   #
