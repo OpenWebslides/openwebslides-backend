d3fa91af29ecf53457a82dda2933fab02793b68e
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-03-07 11:12:28 -0300

    Remove Annotations API remaining references

--- spec/support/policy_sample_context.rb
+++ spec/support/policy_sample_context.rb
@@ -9,33 +9,33 @@ RSpec.shared_context 'policy_sample', :shared_context => :metadata do
       u4 = create :user, :confirmed, :name => 'user4'
 
       # User 1
-      create :topic, :with_assets, :with_conversations, :user => u1, :title => 'u1d1'
-      create :topic, :with_assets, :with_conversations, :user => u1, :title => 'u1d2', :access => :protected
-      create :topic, :with_assets, :with_conversations, :user => u1, :title => 'u1d3', :access => :private
-      user = create :topic, :with_assets, :with_conversations, :user => u1, :title => 'u1d4', :access => :private
+      create :topic, :with_assets, :user => u1, :title => 'u1d1'
+      create :topic, :with_assets, :user => u1, :title => 'u1d2', :access => :protected
+      create :topic, :with_assets, :user => u1, :title => 'u1d3', :access => :private
+      user = create :topic, :with_assets, :user => u1, :title => 'u1d4', :access => :private
       user.collaborators << u2
       user.collaborators << u3
 
       # User 2
-      create :topic, :with_assets, :with_conversations, :user => u2, :title => 'u2d1'
-      create :topic, :with_assets, :with_conversations, :user => u2, :title => 'u2d2', :access => :protected
-      create :topic, :with_assets, :with_conversations, :user => u2, :title => 'u2d3', :access => :private
-      user = create :topic, :with_assets, :with_conversations, :user => u2, :title => 'u2d4', :access => :private
+      create :topic, :with_assets, :user => u2, :title => 'u2d1'
+      create :topic, :with_assets, :user => u2, :title => 'u2d2', :access => :protected
+      create :topic, :with_assets, :user => u2, :title => 'u2d3', :access => :private
+      user = create :topic, :with_assets, :user => u2, :title => 'u2d4', :access => :private
       user.collaborators << u1
       user.collaborators << u3
 
       # User 3
-      create :topic, :with_assets, :with_conversations, :user => u3, :title => 'u3d1'
-      create :topic, :with_assets, :with_conversations, :user => u3, :title => 'u3d2', :access => :protected
-      create :topic, :with_assets, :with_conversations, :user => u3, :title => 'u3d3', :access => :private
-      user = create :topic, :with_assets, :with_conversations, :user => u3, :title => 'u3d4', :access => :private
+      create :topic, :with_assets, :user => u3, :title => 'u3d1'
+      create :topic, :with_assets, :user => u3, :title => 'u3d2', :access => :protected
+      create :topic, :with_assets, :user => u3, :title => 'u3d3', :access => :private
+      user = create :topic, :with_assets, :user => u3, :title => 'u3d4', :access => :private
       user.collaborators << u1
       user.collaborators << u2
 
       # User 4
-      create :topic, :with_assets, :with_conversations, :user => u4, :title => 'u4d1'
-      create :topic, :with_assets, :with_conversations, :user => u4, :title => 'u4d2', :access => :protected
-      create :topic, :with_assets, :with_conversations, :user => u4, :title => 'u4d3', :access => :private
+      create :topic, :with_assets, :user => u4, :title => 'u4d1'
+      create :topic, :with_assets, :user => u4, :title => 'u4d2', :access => :protected
+      create :topic, :with_assets, :user => u4, :title => 'u4d3', :access => :private
     end
   end
 end
