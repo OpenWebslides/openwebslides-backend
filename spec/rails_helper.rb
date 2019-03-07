f5af22a8968414eba9b115fe6b1b9946bf5595fa
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-03-07 09:29:03 -0300

    Deprecate custom matchers in favour of jsonapi-matchers gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,7 +38,9 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.include Headers
   config.include StateMachinesRspec::Matchers
-  config.include OpenWebslides::Matchers
+  config.include Jsonapi::Matchers::Record
+  config.include Jsonapi::Matchers::Attributes
+  config.include Jsonapi::Matchers::Error
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
