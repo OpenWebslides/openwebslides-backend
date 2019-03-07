1740e4d611b4a740e494241d0bc601a0433b0019
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-03-07 11:36:16 -0300

    Rewrite FeedItem API spec

--- spec/requests/feed_item_spec.rb
+++ spec/requests/feed_item_spec.rb
@@ -3,30 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe 'FeedItem API', :type => :request do
-  let(:feed_item) { create :feed_item }
+  ##
+  # Configuration
+  #
+  ##
+  # Stubs and mocks
+  #
+  ##
+  # Subject
+  #
+  subject { response }
 
+  ##
+  # Test variables
+  #
+  ##
+  # Request variables
+  #
+  ##
+  # Tests
+  #
   describe 'GET /' do
-    before do
-    end
+    before { get feed_items_path, :headers => headers }
 
-    it 'returns a list of all @feed_items' do
-      create :feed_item
-      get feed_items_path, :headers => headers
+    prepend_before { create_list :feed_item, 3 }
 
-      expect(response.status).to eq 200
-      expect(response.content_type).to eq "application/vnd.api+json, application/vnd.openwebslides+json; version=#{OpenWebslides.config.api.version}"
-
-      json = JSON.parse response.body
-      expect(json).to include 'data'
-      expect(json['data']).to be_an Array
-      expect(json['data']).not_to be_empty
-
-      feed_item = json['data'].first
-      expect(feed_item).to include 'attributes'
-      expect(feed_item['attributes']).to include 'feedItemType'
-      expect(feed_item['attributes']).to include 'userName'
-      expect(feed_item['attributes']).to include 'topicTitle'
-      expect(feed_item['meta']).to include 'createdAt'
-    end
+    it { is_expected.to have_http_status :ok }
+    it { is_expected.to have_records FeedItem.all }
+    it { is_expected.to have_record_count 3 }
   end
 end
